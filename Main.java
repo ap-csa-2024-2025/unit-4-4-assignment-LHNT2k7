20bbed57a03192394596a140d2e6c96c617801b0
LHNT2k7 <ltri9573@gmail.com>
2024-11-12 10:15:00 -0800

    hgdefs

--- Main.java
+++ Main.java
@@ -4,14 +4,43 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // You can test your method here
-    // System.out.println(isPrime(1));
-    // System.out.println(isPrime(17));
+      String str = "peter piper blah";
+      int count = 0;
+      for (int i = 0, i < str.length()-1; i++)
+      {
+        String pair = input.substring(i, i+2);
+  if (pair.equals("pa") || pair.equals("pe") || pair.equals("pi") || pair.equals("po") || pair.equals("pu"))
+          {
+              count++;
+          }
+      }
   }
+System.out.println("Contains p followed by a vowel " + count + " times."); 
 
+Scanner sc = new Scanner(System.in);
+String input;
+String other = "";
+
+System.out.println("Input String:");
+input = sc.nextLine();
+
+for (int i = 0; i < input.length(); i++)
+{
+  String ch = input.substring(i, i+1);
+  if (! (ch.equals("e") || ch.equals("t") || ch.equals("a") || ch.equals("i") || ch.equals("o")) )
+  {
+    other += ch;
+  }
+}
+System.out.println(other);
+ 
+ 
+ 
+ 
+ 
   public static boolean isPrime(int N)
   {
-    // TODO: Replace and write your code below
+    
     return false;
   }
 }
